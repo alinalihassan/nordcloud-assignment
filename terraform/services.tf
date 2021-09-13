37d9e694f5f0383b8a0867c3b661c44ec9dec210
Alin Ali Hassan <hassanalinali@gmail.com>
2021-09-13 22:07:49 +0200

    Enable Cloud Resource Manager API for Terraform

---
+++ terraform/services.tf
@@ -0,0 +1,6 @@
+# Resource Manager API
+resource "google_project_service" "resource_manager" {
+  service                    = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = true
+}
